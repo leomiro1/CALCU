f14577343da8b40631e992c67ebe572cc5ac38aa
lmiroXside <lmiro@xsidesolutions.com>
2018-11-15 18:23:30 -0300

    1.3: modificacion de tiempos

--- webdriverio-calcu.js
+++ webdriverio-calcu.js
@@ -15,14 +15,14 @@ describe('CALCU Testing', function () {
     });
 
     afterEach(function(){
-        this.timeout(30000);
+        this.timeout(60000);
         return client.click('android=new UiSelector().resourceId("com.candl.athena:id/clear")');
     });
 	
 	//addition testing
 	describe("Test CALCU addition", function () {
         
-		this.timeout(30000);
+		this.timeout(60000);
         
 		it("test-close-startup-screen", function(){
             return client.click('android=new UiSelector().resourceId("com.candl.athena:id/btn_close")');
@@ -74,7 +74,7 @@ describe('CALCU Testing', function () {
 	//multiplication testing
     describe("Test CALCU multiplication", function () {
         
-		this.timeout(30000);
+		this.timeout(60000);
         
 		it("test-positive-multiplication", function () {
             return client.click('android=new UiSelector().resourceId("com.candl.athena:id/digit5")')
@@ -136,7 +136,7 @@ describe('CALCU Testing', function () {
     //division testing
     describe("Test CALCU division", function () {
         
-		this.timeout(30000);
+		this.timeout(60000);
         
 		it("test-divide-even",function(){
             return client.click('android=new UiSelector().resourceId("com.candl.athena:id/digit6")')
